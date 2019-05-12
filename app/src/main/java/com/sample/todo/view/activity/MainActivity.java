90a17e41e402d56baa125370efca56622e4dded9
MirakleYabaze <cool.mirakle@yahoo.com>
2019-05-12 23:46:17 +0530

    dependencies added

--- app/src/main/java/com/sample/todo/view/activity/MainActivity.java
+++ app/src/main/java/com/sample/todo/view/activity/MainActivity.java
@@ -1,11 +1,20 @@
 package com.sample.todo.view.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
 import com.sample.todo.R;
 import com.sample.todo.adapters.ClassDetailRecyclerAdapter;
 import com.sample.todo.adapters.RecyclerViewAdapter;
@@ -20,13 +29,9 @@ import com.sample.todo.view_model.ClassViewModel;
 import com.sample.todo.view_model.SemesterViewModel;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity implements ApiAction.GET_Teacher_CLASS_DETAILS {
 
@@ -48,6 +53,9 @@ public class MainActivity extends AppCompatActivity implements ApiAction.GET_Tea
 
         apiCallPresenter = new ApiCallPresenter(this,this);
         apiCallPresenter.callDetailsAPI();
+
+        callCalendar();
+
         Button fab = findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
@@ -81,6 +89,40 @@ public class MainActivity extends AppCompatActivity implements ApiAction.GET_Tea
         activityMainBinding.setAdapter(recyclerViewAdapter);
 
 
+    }
+
+    private void callCalendar() {
+        final CompactCalendarView compactCalendarView = findViewById(R.id.compactcalendar_view);
+        // Set first day of week to Monday, defaults to Monday so calling setFirstDayOfWeek is not necessary
+        // Use constants provided by Java Calendar class
+        compactCalendarView.setFirstDayOfWeek(Calendar.MONDAY);
+
+        // Add event 1 on Sun, 07 Jun 2015 18:20:51 GMT
+        Event ev1 = new Event(Color.GREEN, 1433701251000L, "Some extra data that I want to store.");
+        compactCalendarView.addEvent(ev1);
+
+        // Added event 2 GMT: Sun, 07 Jun 2015 19:10:51 GMT
+        Event ev2 = new Event(Color.GREEN, 1433704251000L);
+        compactCalendarView.addEvent(ev2);
+
+        // Query for events on Sun, 07 Jun 2015 GMT.
+        // Time is not relevant when querying for events, since events are returned by day.
+        // So you can pass in any arbitary DateTime and you will receive all events for that day.
+        List<Event> events = compactCalendarView.getEvents(1433701251000L); // can also take a Date object
+
+        // events has size 2 with the 2 events inserted previously
+
+        // define a listener to receive callbacks when certain events happen.
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                List<Event> events = compactCalendarView.getEvents(dateClicked);
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+            }
+        });
     }
 
 
